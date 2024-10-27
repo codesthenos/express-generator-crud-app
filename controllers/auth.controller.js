376d65b4a5e6d1c2c47560e64ec54793f5995f2b
codesthenos <codesthenos@gmail.com>
2024-10-27 20:23:26 +0100

    registerController now registers insert users in the database and the response is adapted to be handy for the frontend needs

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,5 @@
+import bcrypt from 'bcryptjs'
+
 import debug from '../debugFunction.js'
 import User from '../models/user.model.js'
 
@@ -5,18 +7,28 @@ export const registerController = async (req, res) => {
   const { userName, email, password } = req.body
   // debug('FIELDS:', '\n', 'username: ', userName, '\n', 'email:', email, '\n', 'password: ', password)
 
+  const hashedPassword = await bcrypt.hash(password, 10)
+
   const newUser = new User({
     userName,
     email,
-    password
+    password: hashedPassword
   })
   // debug('newUser:', '\n', newUser)
 
   try {
+    // throw new Error('FORZADO')
     await newUser.save()
-    res.send(`${newUser}\n\nREGISTERED`)
+    res.status(200).json({
+      id: newUser.id,
+      userName: newUser.userName,
+      email: newUser.email,
+      createdAt: newUser.createdAt,
+      updatedAt: newUser.updatedAt
+    })
     debug('newUser:', '\n', newUser, '\n', 'REGISTERED')
   } catch (error) {
+    res.status(500).json({ message: error.message })
     debug(error)
   }
 }
